abf37c124f2878a044601cc685da788bef69f033
Lucas Rodrigues <lucasfurta@yahoo.com.br>
2022-06-09 12:21:25 -0300

    finalizando o projeto com terraform

--- iac/main.tf
+++ iac/main.tf
@@ -8,17 +8,38 @@ terraform {
 }
 
 provider "digitalocean" {
-  token = "dop_v1_91c0837abaaf1b6cef1176d5dc26b9ef9bb3d24d97b9396519076e0b64aece5d"
+  token = var.do_token
 }
 
 resource "digitalocean_kubernetes_cluster" "k8s-iniciativa" {
-  name   = "k8s-iniciativa"
-  region = "nyc1"
+  name   = var.k8s_name
+  region = var.region
   version = "1.22.8-do.1"
 
   node_pool {
-    name       = "default"
-    size       = "s-2vcpu-2gb"
-    node_count = 3
+    name       = "worker-pool"
+    size       = "s-2vcpu-4gb"
+    node_count = 2
   }
+}
+
+resource "digitalocean_kubernetes_node_pool" "node_premium" {
+  cluster_id = digitalocean_kubernetes_cluster.k8s-iniciativa.id
+
+  name       = "premium"
+  size       = "s-2vcpu-4gb"
+  node_count = 2
+}
+
+variable "do_token" {}
+variable "k8s_name" {}
+variable "region" {}
+
+output "kube_endpoint" {
+  value = digitalocean_kubernetes_cluster.k8s-iniciativa.endpoint
+}
+
+resource "local_file" "kube_config" {
+  content = digitalocean_kubernetes_cluster.k8s-iniciativa.kube_config.0.raw_config
+  filename = "kube_config.yaml"
 }
